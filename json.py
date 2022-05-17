c32f40d7eb4c19bd39e202e36a38c344466a9413
celc94 <ces94.celc@gmail.com>
2022-05-16 22:17:53 -0500

    diccionarios2

---
+++ json.py
@@ -0,0 +1,12 @@
+{
+"Nombre": "Douglas",
+"Apellido": "Crockford",
+"pasatiempos": ["trotar", "bucear", "cantar"],
+"edad": 64,
+"empleado": False,
+"jefe": None,
+"hijos": [
+{"Nombre": "Alice", "edad": 16},
+{"Nombre": "Bob", "edad": 8}
+]
+}
