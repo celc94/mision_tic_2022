ab39e6e1108ac5f6c69f338739b90a521090f09a
celc94 <ces94.celc@gmail.com>
2022-09-01 10:27:49 -0500

    Se cambia archivo a proyecto cooperativa

---
+++ ciclo3/semana2/proyectoCooperativa/cooperativa/appcooperativa/models.py
@@ -0,0 +1,25 @@
+from ast import mod
+from django.db import models #las tablas se crean directamente en app
+
+# Create your models here.
+class Cliente(models.Model): #Hereda unas funciones de una superclase Model
+#Atributos de la clase
+    documento = models.IntegerField(primary_key=True) #Llave Primaria
+    nombre = models.CharField(max_length=30) #30 caracteres
+    apellido = models.CharField(max_length=30)
+    correo = models.CharField(max_length=20)
+    celular = models.CharField(max_length=15)
+
+class Lineas_De_Credito(models.Model):
+    codigo = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=30)
+    plazomax = models.IntegerField()
+    montomax = models.IntegerField()
+
+class Credito(models.Model):
+    codigo_credito = models.IntegerField(primary_key=True)
+    monto_prestado = models.IntegerField()
+    fecha_credito = models.DateField() #Para fecha DateField
+    #Llave foranea, documento del cliente
+    documento = models.ForeignKey(Cliente, on_delete=models.CASCADE) #Si elimino un dato en la tabla credito, se elimna en la tabla cliente
+    codigo = models.ForeignKey(Lineas_De_Credito, on_delete=models.CASCADE)
